f07e1f8acf85d64ead82fa71e68c5af6b033e8f2
h-kwnm <63909809+h-kwnm@users.noreply.github.com>
2026-05-12 11:38:09 +0900

    add read limit to be consistent with the others

--- fetcher_rfc6962.go
+++ fetcher_rfc6962.go
@@ -76,7 +76,7 @@ func fetchProofByHash(h string, log *CachedLog) (RFC6962ProofResult, error) {
 
 	ts := time.Now().UTC()
 
-	body, err := io.ReadAll(resp.Body)
+	body, err := io.ReadAll(io.LimitReader(resp.Body, 1<<20))
 	if err != nil {
 		return RFC6962ProofResult{}, err
 	}
