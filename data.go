52e8ade9d7c8c3d80ffd763d8d592210e5033173
h-kwnm <63909809+h-kwnm@users.noreply.github.com>
2026-05-15 17:09:52 +0900

    add a missing os.Exit(1) to runGetData

--- data.go
+++ data.go
@@ -54,6 +54,7 @@ func runData(args []string) {
 	}
 	if !found {
 		fmt.Fprintf(os.Stderr, "leaf index %d not found in tile. note that the latest leaf index is (treeSize - 1) since tree size is 1-based and leaf index is 0-based\n", *index)
+		os.Exit(1)
 	}
 
 	if *outpath != "" {
